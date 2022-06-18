657525a64d6854f23ccacdc147901cbaec2d2ac9
Jessica Taylor <96968757+JessLTaylor@users.noreply.github.com>
2022-06-17 22:54:12 -0400

    agrparse updates

---
+++ blood_donation_clinic/__init__.py
@@ -0,0 +1,11 @@
+# Add import statements for objects you want to make for easy import
+# EXAMPLE: 
+#
+#    from myproject.myproject import cool_function
+#
+# so that user can do the following in their notebook or script:
+#
+#    from myproject import cool_function
+
+from blood_donation_clinic.blood_donation_clinic_model import simulate
+from blood_donation_clinic.blood_donation_clinic_model import process_sim_output
